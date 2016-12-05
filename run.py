5ee1aab2bd03f8823f569b23480da1d772ebd0e3
Jean-Luc CANELA <jlcanelam@gmail.com>
2016-12-05 14:18:13 +0100

    update from developer

---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from server.app import app
+
+def main():
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+if __name__ == '__main__':
+    main()
---
+++ server/app.py
@@ -0,0 +1,51 @@
+import re
+from copy import deepcopy
+import json
+
+from pprint import pprint
+
+from flask import Flask, request, jsonify, abort
+
+app = Flask(__name__)
+
+
+class OrderException(Exception): pass
+
+
+def get_quote(order):
+    try:
+        value = 1.8 * order['nb_days'] * order['nb_travelers']
+    except Exception:
+        raise OrderException('invalid order %s' % order)
+
+    quote = {'quote': value}
+    return quote
+
+@app.route('/quote', methods=['POST'])
+def quote():
+    data = deepcopy(request.data)
+
+    print 'POST data:'
+    pprint(data)
+
+    try:
+        order = json.loads(data)
+    except ValueError:
+        abort(400)
+    try:
+        quote = get_quote(order)
+    except OrderException, e:
+        abort(400)
+
+    return jsonify(quote)
+
+@app.route('/feedback', methods=['POST'])
+def feedback():
+    # TODO: do something!
+    return 'Thank you dude, you rule!'
+
+
+@app.route("/", methods=['GET'])
+@app.route("/<path:path>", methods=['GET'])
+def test(path=''):
+    return 'OK'
