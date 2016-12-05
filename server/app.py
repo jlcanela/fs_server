bceffac198a8b522bb16bdfeb2584efc5b03e5cc
Jerome Clerc <jererc@gmail.com>
2016-12-05 17:11:44 +0100

    fix post data

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
-import re
-from copy import deepcopy
 import json
+from datetime import datetime
 
 from pprint import pprint
 
@@ -11,16 +10,18 @@ app = Flask(__name__)
 
 class OrderException(Exception): pass
 
+def get_date(date_str):
+    return datetime.strptime(date_str, '%Y-%m-%d')
 
 def get_quote(order):
     try:
-        value = 1.8 * order['nb_days'] * order['nb_travelers']
-    except Exception as e:
-        pprint(e)
-        raise OrderException('invalid order %s' % order)
-
-    quote = {'quote': value}
-    return quote
+        delta = get_date(order['returnDate']) - get_date(order['departureDate'])
+        print 'nb days:', delta.days
+        print 'nb travelers:', len(order['travellerAges'])
+        value = 1.8 * delta.days * len(order['travellerAges'])
+    except Exception, e:
+        raise OrderException('invalid order %s: %s' % (order, str(e)))
+    return {'quote': value}
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/<path:path>", methods=['GET'])
@@ -29,20 +30,20 @@ def index2(path=''):
 
 @app.route('/quote', methods=['POST'])
 def quote():
-    data = deepcopy(request.data)
+    data = request.get_data()
 
     print 'POST data:'
     pprint(data)
 
     try:
         order = json.loads(data)
-    except ValueError as e:
-        pprint(e)
+    except ValueError, e:
+        print str(e)
         abort(400)
     try:
         quote = get_quote(order)
     except OrderException, e:
-        pprint(e)
+        print str(e)
         abort(400)
 
     return jsonify(quote)
